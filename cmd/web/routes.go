889230d6b1679079ce0ffb28ca959ac9afe94e7e
frog-in-fog <yrlovn1999@gmail.com>
2023-09-05 20:00:47 +0300

    repository first change 05.09.23

---
+++ cmd/web/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/payment-succeeded", app.PaymentSucceeded)
+
+	fileserver := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileserver))
+
+	return mux
+}
